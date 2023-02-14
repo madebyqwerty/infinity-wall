2c563be20d4e23326160b463cb82d1b8d21390f4
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-14 18:44:20 +0100

    Fix Vytvořit přihlašování pro uživatele #26

--- web/src/routes/auth/login/+page.svelte
+++ web/src/routes/auth/login/+page.svelte
@@ -10,35 +10,14 @@
 </script>
 
 <div class="hero min-h-screen bg-base-200">
-	{#if unauthorized}
-		<div class="toast toast-top toast-end">
-			<div class="alert alert-error shadow-lg">
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="stroke-current flex-shrink-0 h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-						/></svg
-					>
-					<span>{unauthorized}</span>
-				</div>
-			</div>
-		</div>
-	{/if}
 	<div class="hero-content flex-col lg:flex-row-reverse gap-16">
 		<div class="text-center lg:text-left">
 			<h1 class="text-5xl font-bold">
-				Přihlašte se do <span class="text-primary">UpShop {site_name}</span>
+				Přihlašte se do <br /> <span class="text-primary">UpShop {site_name}</span>
 			</h1>
 			<p class="py-6 lg:max-w-md">
-				Vítejte v naší aplikaci pro zaznamenávání vašeho osobního růstu a profesionálního rozvoje v
-				rámci společnosti UpShop. Prosím, přihlaste se a začněte využívat všech možností, které vám
+				Vítejte v aplikaci pro zaznamenávání vašeho osobního růstu a profesionálního rozvoje v rámci
+				naší společnosti UpShop. Prosím, přihlaste se a začněte využívat všech možností, které vám
 				naše aplikace nabízí. Jsme tu pro vás, abychom vám pomohli s vaším růstem a abychom vás
 				podpořili na cestě k dosažení vašich cílů.
 			</p>
@@ -50,6 +29,11 @@
 		>
 			<div class="card-body">
 				<div class="form-control">
+					{#if unauthorized}
+						<div class="text-error">
+							{unauthorized}
+						</div>
+					{/if}
 					<label class="label" for="email">
 						<span class="label-text">Email</span>
 					</label>
