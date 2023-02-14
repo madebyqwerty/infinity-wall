c6d6c5076e78762265127ffaf363247c0ba11490
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-14 17:36:39 +0000

    Vylepšení form errorů

--- web/src/routes/auth/login/+page.server.ts
+++ web/src/routes/auth/login/+page.server.ts
@@ -1,11 +1,35 @@
-import { error, redirect } from '@sveltejs/kit';
+import { error, fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import { zfd } from 'zod-form-data';
+import { z } from 'zod';
 
 export const actions: Actions = {
 	default: async ({ locals, request }) => {
-		const { email, password } = Object.fromEntries(await request.formData());
+		const data = await request.formData();
+		const { email, password } = Object.fromEntries(data);
 
-		await locals.pb.collection('users').authWithPassword(email as string, password as string);
+		const loginSchema = zfd.formData({
+			email: z.string({ required_error: 'Email nesmí být prázdný' }),
+			password: z
+				.string({ required_error: 'Heslo nesmí být prázdné' })
+				.min(8, 'Heslo musí být mít minimálně 8 znaků')
+		});
+
+		const result = loginSchema.safeParse(data);
+
+		if (!result.success) {
+			const response = {
+				errors: result.error.flatten().fieldErrors
+			};
+
+			return fail(400, response);
+		}
+
+		try {
+			await locals.pb.collection('users').authWithPassword(email as string, password as string);
+		} catch (e) {
+			return fail(403, { error: 'Špatné heslo nebo email' });
+		}
 
 		throw redirect(303, '/');
 	}
--- web/src/routes/auth/login/+page.svelte
+++ web/src/routes/auth/login/+page.svelte
@@ -1,9 +1,36 @@
-<script>
+<script lang="ts">
 	import { enhance } from '$app/forms';
 	import { site_name } from '@utils';
+	import type { FormData } from './$types';
+
+	export let form: FormData;
+	$: email_error = form?.errors?.email?.at(0);
+	$: password_error = form?.errors?.password?.at(0);
+	$: unauthorized = form?.error;
 </script>
 
 <div class="hero min-h-screen bg-base-200">
+	{#if unauthorized}
+		<div class="toast toast-top toast-end">
+			<div class="alert alert-error shadow-lg">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-current flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+					<span>{unauthorized}</span>
+				</div>
+			</div>
+		</div>
+	{/if}
 	<div class="hero-content flex-col lg:flex-row-reverse gap-16">
 		<div class="text-center lg:text-left">
 			<h1 class="text-5xl font-bold">
@@ -11,8 +38,8 @@
 			</h1>
 			<p class="py-6 lg:max-w-md">
 				Vítejte v naší aplikaci pro zaznamenávání vašeho osobního růstu a profesionálního rozvoje v
-				rámci společnosti UpShop. Prosím, přihlaste se níže a začněte využívat všech možností, které
-				vám naše aplikace nabízí. Jsme tu pro vás, abychom vám pomohli s vaším růstem a abychom vás
+				rámci společnosti UpShop. Prosím, přihlaste se a začněte využívat všech možností, které vám
+				naše aplikace nabízí. Jsme tu pro vás, abychom vám pomohli s vaším růstem a abychom vás
 				podpořili na cestě k dosažení vašich cílů.
 			</p>
 		</div>
@@ -30,14 +57,27 @@
 						type="text"
 						placeholder="martin.novak@upshop.cz"
 						id="email"
-						class="input input-bordered"
+						name="email"
+						class="input input-bordered {email_error || unauthorized ? 'input-error' : ''}"
 					/>
+					{#if email_error}
+						<span class="text-error">{email_error}</span>
+					{/if}
 				</div>
 				<div class="form-control">
 					<label class="label" for="password">
 						<span class="label-text">Heslo</span>
 					</label>
-					<input type="text" placeholder="*******" class="input input-bordered" id="password" />
+					<input
+						type="password"
+						placeholder="*******"
+						name="password"
+						class="input input-bordered {password_error || unauthorized ? 'input-error' : ''}"
+						id="password"
+					/>
+					{#if password_error}
+						<span class="text-error">{password_error}</span>
+					{/if}
 				</div>
 				<div class="form-control mt-6">
 					<button class="btn btn-primary" type="submit">Přihlásit se</button>
