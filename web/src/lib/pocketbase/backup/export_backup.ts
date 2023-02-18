2691d4a513a478785d4902fc37d033209469ad08
Venca321 <login@vaclavparma.cz>
2023-02-18 10:33:22 +0100

    export backup funguje

--- web/src/lib/pocketbase/backup/export_backup.ts
+++ web/src/lib/pocketbase/backup/export_backup.ts
@@ -1,15 +1,21 @@
-export function export_backup() {
-	const rows = [
-		['name1', 'city1', 'some other info'],
-		['name2', 'city2', 'more info']
-	];
+import { element } from 'svelte/internal';
+import { pb } from '../index';
 
-	let csvContent = 'data:text/csv;charset=utf-8,' + rows.map((e) => e.join(',')).join('\n');
+export async function export_backup() {
+	const records = await pb.collection('records').getFullList(200 /* batch size */, {
+		sort: '-created'
+	});
+
+	const out: any[][] = [];
+	records.forEach((element) => {
+		out.push(Object.values(element));
+	});
+
+	let csvContent = 'data:text/csv;charset=utf-8,' + out.map((e) => e.join(',')).join('\n');
 	var encodedUri = encodeURI(csvContent);
 	var link = document.createElement('a');
 	link.setAttribute('href', encodedUri);
 	link.setAttribute('download', 'backup.csv');
-	document.body.appendChild(link); // Required for FF
-
-	link.click(); // This will download the data file named "my_data.csv".
+	document.body.appendChild(link);
+	link.click();
 }
