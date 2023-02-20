eb8a7bd3e58dcd2a728fd8a2d331ab9f48d9c762
MonteDan <bacak.rangercz@gmail.com>
2023-02-20 17:13:26 +0100

    page.server.ts

--- web/src/routes/(app)/addrecord/+page.svelte
+++ web/src/routes/(app)/addrecord/+page.svelte
@@ -16,24 +16,26 @@ createRecord(data.length, data.language, data.description, data.date, data.ratin
 $:console.log(data.rating)
   </script>
 
-<form action="">
+<form use:enhance method="POST">
+  <DateInput bind:value={data.date}  closeOnSelection={true}/>
     <input type="number" placeholder="Délka" class="input input-bordered w-full max-w-xs" />
+    <div class="rating rating-lg">
+      <input type="radio" name="rating-9" bind:group={data.rating} value={1} class="mask mask-star-2" />
+      <input type="radio" name="rating-9" bind:group={data.rating} value={2} class="mask mask-star-2" />
+      <input type="radio" name="rating-9" bind:group={data.rating} value={3} class="mask mask-star-2" />
+      <input type="radio" name="rating-9" bind:group={data.rating} value={4} class="mask mask-star-2" />
+      <input type="radio" name="rating-9" bind:group={data.rating} value={5} class="mask mask-star-2" />
+  </div>
     <select class="select select-bordered w-full max-w-xs" bind:value={data.language}>
         <option disabled selected>Vyberte Jazyk</option>
         {#each languageNames as l}
             <option value={l}>{l}</option>
         {/each}
     </select>
-    <div class="rating rating-lg">
-        <input type="radio" name="rating-9" bind:group={data.rating} value={1} class="mask mask-star-2" />
-        <input type="radio" name="rating-9" bind:group={data.rating} value={2} class="mask mask-star-2" />
-        <input type="radio" name="rating-9" bind:group={data.rating} value={3} class="mask mask-star-2" />
-        <input type="radio" name="rating-9" bind:group={data.rating} value={4} class="mask mask-star-2" />
-        <input type="radio" name="rating-9" bind:group={data.rating} value={5} class="mask mask-star-2" />
-    </div>
+    
     <textarea class="textarea textarea-bordered" placeholder="Bio" bind:value={data.description}></textarea>
 
-    <DateInput bind:value={data.date}  closeOnSelection={true}/>
+
     <button on:click={add}>
     Přidat záznam
     </button>
