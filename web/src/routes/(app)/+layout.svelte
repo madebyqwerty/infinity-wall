3ee1a218bc4fdeb383d21a29e910eaa3413806f2
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-21 21:05:49 +0100

    Upravit homepage

--- web/src/routes/(app)/+layout.svelte
+++ web/src/routes/(app)/+layout.svelte
@@ -1,1 +1,24 @@
-<slot />
+<script lang="ts">
+	import Dashboard from './Dashboard.svelte';
+	import DrawerToggle from '@components/DrawerToggle.svelte';
+	import Navbar from './Navbar.svelte';
+</script>
+
+<main class="min-h-screen">
+	<Navbar />
+	<div class="drawer">
+		<DrawerToggle routes={['create']} />
+		<div class="drawer-content">
+			<Dashboard />
+		</div>
+		<slot />
+	</div>
+</main>
+
+<style>
+	/* Tohle je tady aby tam nebyl divný flashing před tím než se loadne daisyui theme */
+	main {
+		min-width: 100vv;
+		min-height: 100vh;
+	}
+</style>
