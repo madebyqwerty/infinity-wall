ea037d009f404893cefc550d08caeedd22c16bbf
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-02-23 19:43:30 +0100

    Přidat hooks.server.ts auth kontrolu

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,6 +1,8 @@
-import type { Handle } from '@sveltejs/kit';
-import PocketBase from 'pocketbase';
+import { redirect, type Handle } from '@sveltejs/kit';
+import PocketBase, { type Admin, type Record } from 'pocketbase';
 import { pocketbase_URL } from '@pocketbase';
+
+const is_admin = (user: Record | Admin | null) => user?.collectionName === 'admins';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase(pocketbase_URL);
@@ -20,6 +22,25 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	event.locals.user = structuredClone(event.locals.pb.authStore.model);
+
+	let is_valid = event.locals.pb.authStore.isValid;
+
+	// This code checks if the user is logged in or not
+	// If the user is not logged in, it redirects to the login page
+	// Depending on the page, it redirects to the admin login page or the user login page
+	// If the user is logged in, it does nothing
+
+	if (!event.url.toString().includes('auth') && !is_valid) {
+		const url = event.url.toString();
+		const redirect_url = url.includes('admin') ? '/auth/login' : '/auth/login/admin';
+		throw redirect(303, redirect_url);
+	}
+
+	// If the user is not an admin, redirect them to the homepage
+	if (event.url.toString().includes('admin') && !is_admin(event.locals.user)) {
+		throw redirect(303, '/');
+	}
+
 	const response = await resolve(event);
 
 	response.headers.set(
