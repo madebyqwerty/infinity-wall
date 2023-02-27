daef80adaf9131c6bb192e0da71652a86d434be8
MonteDan <bacak.rangercz@gmail.com>
2023-02-27 15:54:43 +0100

    dalsi malej fix

--- web/src/routes/(app)/Hero.svelte
+++ web/src/routes/(app)/Hero.svelte
@@ -11,11 +11,12 @@
 			>{new Date($page.data.date_past).toLocaleDateString('cs')}</span
 		>
 		do <span class="highlighted">{new Date($page.data.date_end).toLocaleDateString('cs')}</span>
-		jste si {$page.data.records.length==0?"nepřidal/a žádný":"přidal/a"} <span class="highlighted">{$page.data.records.length} záznam{[2,3,4].includes($page.data.records.length)?"y":[0,1].includes($page.data.records.length)?"":"ů"}</span>. Váše
+		jste si {$page.data.records.length==0?"nepřidal/a žádný":"přidal/a"} <span class="highlighted">{$page.data.records.length} záznam{[2,3,4].includes($page.data.records.length)?"y":[0,1].includes($page.data.records.length)?"":"ů"}</span>.
+		{#if $page.data.records.length>0} Váše
 		průměrna doba tréninku byla
 		<span class="highlighted">
 			{Math.round($page.data.usage_data.totalTime/$page.data.records.length)} minut.
-		</span>
+		</span>{/if}
 	</p>
 	{#if $page.data.records.length>0}
 	<h3>Nejpoužívanější jazyky</h3>
