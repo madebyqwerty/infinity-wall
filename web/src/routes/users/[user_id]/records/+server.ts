4ccdd24ae8a3c647cbadfaee2edc15482938a3c1
Venca321 <login@vaclavparma.cz>
2023-03-01 16:44:33 +0100

    API snad hotovo

--- web/src/routes/users/[user_id]/records/+server.ts
+++ web/src/routes/users/[user_id]/records/+server.ts
@@ -1,6 +1,7 @@
 
 import { pb } from '@pocketbase';
 import { get_date_in_ddmmyyyy, get_date_from_ddmmyyyy } from '@utils/dates';
+import { from_hours_and_minutes, to_hours_and_minutes } from '@utils/time';
 
 pb.admins.authWithPassword("login@vaclavparma.cz", "testtesttest")
 
@@ -19,7 +20,7 @@ export const GET = async ({ params }) => {
             out.push({
                 "id": id,
                 "date": date,
-                "time-spent": time.toString(),
+                "time-spent": to_hours_and_minutes(time),
                 "programming-language": language,
                 "rating": rating,
                 "description": description
@@ -40,13 +41,13 @@ export const POST = async ({ request, params }) => {
         const { user_id } = params;
         const body = await request.json()
 
-        const created = await pb.collection('records').create({ "time": parseInt(body["time-spent"]), "rating": parseInt(body["rating"]), "description": body["description"], "user": user_id, "date": get_date_from_ddmmyyyy(body["date"]).toISOString(), "language": body["programming-language"] });
+        const created = await pb.collection('records').create({ "time": from_hours_and_minutes(body["time-spent"]), "rating": parseInt(body["rating"]), "description": body["description"], "user": user_id, "date": get_date_from_ddmmyyyy(body["date"]).toISOString(), "language": body["programming-language"] });
 
         const record = await pb.collection('records').getOne(created.id, {});
 
         const { id, date, time, language, rating, description } = record;
 
-        return new Response(JSON.stringify({ id: id, date: get_date_in_ddmmyyyy(new Date(date)), "time-spent": time.toString(), "programming-language": language, rating: rating, description: description }), { status: 200 });
+        return new Response(JSON.stringify({ id: id, date: get_date_in_ddmmyyyy(new Date(date)), "time-spent": to_hours_and_minutes(time), "programming-language": language, rating: rating, description: description }), { status: 200 });
     }
     catch {
         return new Response(JSON.stringify(""), { status: 404 });
