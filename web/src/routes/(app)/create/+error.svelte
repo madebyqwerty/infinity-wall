5ae671f00dcece247890e71e953ab15a67f33a6a
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-03-03 16:00:08 +0100

    Přidat error page

---
+++ web/src/routes/(app)/create/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Sidebar from '@components/Sidebar.svelte';
+</script>
+
+<Sidebar route="/home" title="Jejda, něco se pokazilo :(">
+	Omlouváme se ale nastal nějaký problém. Zkuste to prosím znovu.
+</Sidebar>
