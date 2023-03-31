ff0fdeea49594f61d49b7a92e6b68202f1d22e2e
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-03-31 18:50:04 +0200

    xddd

--- web/src/routes/NoteInner.svelte
+++ web/src/routes/NoteInner.svelte
@@ -5,6 +5,7 @@
 	import { pb } from '@pocketbase';
 	import { editing } from '@utils/editingstore';
 	import { invalidateAll } from '$app/navigation';
+	import CharacterCount from '@tiptap/extension-character-count';
 
 	export let id;
 	export let body: string;
@@ -13,14 +14,25 @@
 	let element;
 	let editor;
 
+	let onlyText = '';
+
+	const limit = 120;
+
+	$: length = onlyText.length;
+
 	let isEdit = false;
 
 	onMount(() => {
 		editor = new Editor({
 			element: element,
-			extensions: [StarterKit],
+			extensions: [
+				StarterKit,
+				CharacterCount.configure({
+					limit
+				})
+			],
 			content: body ? JSON.parse(body) : '',
-			editable: true,
+			editable: length <= 120,
 			editorProps: {
 				attributes: {
 					class: 'focus:outline-none focus:ring-0 p-2'
@@ -34,6 +46,8 @@
 					{ body: JSON.stringify(editor.getJSON()) },
 					{ $autoCancel: false }
 				);
+
+				onlyText = editor.getText();
 			},
 			onFocus: () => {
 				$editing = true;
@@ -56,13 +70,13 @@
 	}
 </script>
 
-<div class="card-body p-4">
+<div class="card-body p-4 max-w-[15rem]">
 	<div class="focus:outline-none focus:ring-0" bind:this={element} />
 
 	<em>-{author}</em>
 
 	<div class="card-actions">
-		<button class="btn btn-circle btn-ghost btn-sm" on:click={handleDelete}>
+		<button class="btn btn-circle btn-ghost btn-sm z-10" on:click={handleDelete}>
 			<iconify-icon icon="material-symbols:delete-outline" />
 		</button>
 	</div>
